25108a66ede8760c5bdd031fac114d5d6b65f420
David <dvd.pansardis@gmail.com>
2016-09-22 18:29:15 -0300

    set url

--- js/controllers/PanelMktCtrl.js
+++ js/controllers/PanelMktCtrl.js
@@ -3,7 +3,7 @@ var PanelMktCtrl = function() {
 	var self;
 	var PanelMktCtrl = function() {};
 
-    var domain = "http://127.0.0.1:8887/teste_gauge/";
+    var domain = "http://dvdpansardis.github.io/teste_gauge/";
 
     var listBrands = [];
     var listInteractions = [];
